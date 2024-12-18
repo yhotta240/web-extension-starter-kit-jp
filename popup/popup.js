52a776c90d3ad0ea7897d2500932bb58d3b1a5ea
yhotta240 <yhotta240@icloud.com>
2024-12-19 01:41:46 +0900

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,10 @@
 // 初期化処理
 let isEnabled = false; // ツールの有効状態を示すフラグ（初期値はfalse）
 const enabledElement = document.getElementById('enabled'); // チェックボックス（トグルボタン）要素を取得
-const panelButton  = document.getElementById('panelButton');
+const panelButton = document.getElementById('panelButton');
 const messagePanel = document.getElementById('messagePanel');
 const messageDiv = document.getElementById('message'); // メッセージ表示用のdiv要素を取得
-
+const manifestData = chrome.runtime.getManifest();
 
 // チェックボックス（トグルボタン）の状態が変更されたとき，ツールの有効/無効状態を更新
 enabledElement.addEventListener('change', (event) => {
@@ -13,7 +13,7 @@ enabledElement.addEventListener('change', (event) => {
   // 現在の有効/無効状態をストレージに保存
   chrome.storage.local.set({ isEnabled: isEnabled }, () => {
     // 有効/無効状態に応じてメッセージを出力
-    messageOutput(dateTime(), isEnabled ? 'Sampleは有効になっています' : 'Sampleは無効になっています');
+    messageOutput(dateTime(), isEnabled ? `${manifestData.name} は無効になっています` : `${manifestData.name} は無効になっています`);
   });
 });
 
@@ -25,7 +25,7 @@ chrome.storage.local.get(['settings', 'isEnabled'], (data) => {
     enabledElement.checked = isEnabled; // チェックボックス（トグルボタン）の状態を'isEnabled'の値に設定
   }
   // 有効/無効状態に応じてメッセージを出力
-  messageOutput(dateTime(), isEnabled ? 'Sampleは有効になっています' : 'Sampleは無効になっています');
+  messageOutput(dateTime(), isEnabled ? `${manifestData.name} は無効になっています` : `${manifestData.name} は無効になっています`);
 });
 
 
@@ -40,11 +40,11 @@ document.addEventListener('DOMContentLoaded', function () {
     if (messagePanel.style.height === panelHeight) {
       // パネルが開いている場合は閉じる
       messagePanel.style.height = '0';
-      panelButton.textContent   = 'メッセージパネルを開く';
+      panelButton.textContent = 'メッセージパネルを開く';
     } else {
       // パネルが閉じている場合は開く
       messagePanel.style.height = panelHeight;
-      panelButton.textContent   = 'メッセージパネルを閉じる';
+      panelButton.textContent = 'メッセージパネルを閉じる';
     }
   });
 
@@ -53,7 +53,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const storeLink = document.getElementById('store_link');
   if (storeLink) clickURL(storeLink);
   // manifest.jsonから拡張機能の情報を取得
-  const manifestData = chrome.runtime.getManifest();
   // 各情報をHTML要素に反映
   document.getElementById('extension-id').textContent = `${chrome.runtime.id}`;
   document.getElementById('extension-name').textContent = `${manifestData.name}`;
@@ -86,7 +85,8 @@ document.addEventListener('DOMContentLoaded', function () {
 // 設定をストレージに保存する関数
 function saveSettings(datetime, message, value) {
   const settings = {
-    blurValue: value, // ぼかしの設定値
+    sampleValue: value,
+    // storageに保存するための設定をここに追加する
   };
 
   // ストレージに設定を保存し，保存完了後にメッセージを出力
